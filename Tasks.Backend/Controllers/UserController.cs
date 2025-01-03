4e59152aeabf0b70654287e3444a712dc46236fe
lobaofernando <lobaofernando15@gmail.com>
2025-01-03 20:14:32 -0300

    adaptação do usercontroller para utilizar userservice e implementação do restante do crud

--- Tasks.Backend/Controllers/UserController.cs
+++ Tasks.Backend/Controllers/UserController.cs
@@ -1,7 +1,4 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Tasks.Backend.Data;
+﻿using Microsoft.AspNetCore.Mvc;
 using Tasks.Backend.DTOs;
 using Tasks.Backend.Models;
 using Tasks.Backend.Services;
@@ -12,14 +9,10 @@ namespace Tasks.Backend.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
-        private readonly IMapper _mapper;
         private readonly UserService _userService;
 
-        public UserController(ApplicationDbContext context, IMapper mapper, UserService userService)
+        public UserController(UserService userService)
         {
-            _context = context;
-            _mapper = mapper;
             _userService = userService;
         }
 
@@ -27,7 +20,7 @@ namespace Tasks.Backend.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllUsers()
         {
-            var users = _userService.GetAllUsersAsync();
+            var users = await _userService.GetAllUsersAsync();
 
             return Ok(users);
         }
@@ -54,12 +47,12 @@ namespace Tasks.Backend.Controllers
             var existingUser = await _userService.ExistingUser(user.Email);
             if (existingUser)
             {
-                return BadRequest(new { message = "O e-mail já está em uso." });
+                return Conflict(new { message = "O e-mail já está em uso." });
             }
 
             if (_userService.CreateUser(user) == null)
             {
-                return NotFound();
+                return StatusCode(500, new {message = "Erro na criação do usuário" });
             }
 
             return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
@@ -74,34 +67,32 @@ namespace Tasks.Backend.Controllers
                 return BadRequest(new { message = "O ID do usuário não corresponde." });
             }
 
-            var user = await _context.Users.FindAsync(id);
+            if (await _userService.ExistingUser(updatedUser.Email))
+            {
+                return Conflict("E-mail já cadastrado");
+            }
+
+            var user = _userService.UpdateUser(id, updatedUser);
+
             if (user == null)
             {
                 return NotFound(new { message = "Usuário não encontrado." });
             }
 
-            user.Name = updatedUser.Name;
-            user.Email = updatedUser.Email;
-            user.Password = updatedUser.Password; // Não recomendado em produção sem criptografia
-
-            _context.Entry(user).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-            return NoContent();
+            return Ok();
         }
 
         // DELETE: api/User/{id}
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
+            var user = await _userService.DeleteUserAsync(id);
+            if (user == false)
             {
                 return NotFound(new { message = "Usuário não encontrado." });
             }
 
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
-            return NoContent();
+            return Ok();
         }
     }
 }
--- Tasks.Backend/Services/UserService.cs
+++ Tasks.Backend/Services/UserService.cs
@@ -4,6 +4,7 @@ using Tasks.Backend.DTOs;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.Intrinsics.Arm;
+using Microsoft.VisualBasic;
 
 namespace Tasks.Backend.Services
 {
@@ -48,6 +49,45 @@ namespace Tasks.Backend.Services
             await _context.SaveChangesAsync();
 
             return _mapper.Map<UserDTO>(user);
+        }
+
+        public async Task<UserDTO> UpdateUser(int id, User updateUser)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return null;
+            }
+
+            user.Name = updateUser.Name;
+            user.Email = updateUser.Email;
+
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<UserDTO>(user);
+        }
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if(user == null) return false;
+
+            try
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new InvalidOperationException("Erro ao excluir o usuário:" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro inesperado");
+            }
         }
     }
 }
